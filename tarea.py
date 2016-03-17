279c694768128c1fe7d0e64a86be7c6a743a20e0
Hackerter <hackerter@gmail.com>
2016-03-17 12:46:04 -0300

    archivos modificados

--- tarea.py
+++ tarea.py
@@ -1,1 +1,13 @@
+#-*- coding: utf-8 -*-
+
 #archivo python de prueba
+
+def comparar(x,y):
+	if x>y return x
+	elif y>x return y
+	else	return "Iguales"
+
+ if __name__ == "__main__":
+	x = input("Ingrese un numero entero")
+	y = input("Ingrese un numero entero")
+	print (comparar(x,y))
