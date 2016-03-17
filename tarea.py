f09117ba331caefd330f51a24da9ebe0e60e6783
hackerter <hackerter@gmail.com>
2016-03-17 14:50:30 -0300

    tarea 1, terminada

--- tarea.py
+++ tarea.py
@@ -1,13 +1,22 @@
 #-*- coding: utf-8 -*-
 
-#archivo python de prueba
+#archivo python que compara 2 numeros si son mayor, menor e iguales
 
 def comparar(x,y):
-	if x>y return x
-	elif y>x return y
-	else	return "Iguales"
+	if x>y:
+	  s1 = 'Mayor '+str(x)
+	  s2 = 'Menor '+str(y)
+	  print ("!")
+	elif y>x:
+	  s1 = 'Mayor '+ str(y)
+	  s2 = 'Menor '+ str(x)
+	  print ("#")
+	else:	
+	  return "Iguales"
+	return (s1+' Y '+s2)
+	
 
- if __name__ == "__main__":
-	x = input("Ingrese un numero entero")
-	y = input("Ingrese un numero entero")
-	print (comparar(x,y))
+if __name__ == "__main__":
+	x = input("Ingrese primer numero entero: ")
+	y = input("Ingrese segundo numero entero: ")
+	print ("Resultado "+ comparar(x,y))
